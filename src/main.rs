d37ca6aa909d7e458f115b3786f52b38f4d77081
DavePearce <dave01001110@gmail.com>
2023-10-10 16:41:35 +1300

    Working on writing out file groups
    This is putting in place a general mechanism for printing out "groups"
    of blocks into separate files.  However, at this stage, the mechanism
    for dividing a contract into groups is very primitive.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod opcodes;
 mod printer;
 
 use std::fs;
+use std::collections::HashMap;
 use std::error::Error;
 use clap::{Arg, Command};
 use evmil::analysis::{insert_havocs,trace};
@@ -12,7 +13,7 @@ use evmil::bytecode::Instruction::*;
 use evmil::util::{FromHexString,ToHexString};
 
 use analysis::{State};
-use block::{BlockSequence};
+use block::{Block,BlockSequence};
 use printer::*;
 
 
@@ -26,6 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
              .value_name("SIZE")
              .value_parser(clap::value_parser!(usize))
              .default_value("65535"))
+        .arg(Arg::new("split").long("split").value_name("json-file"))
         .arg(Arg::new("target").required(true))        
         .get_matches();
     // Extract arguments
@@ -36,9 +38,116 @@ fn main() -> Result<(), Box<dyn Error>> {
     let bytes = fs::read(target)?;    
     // Generate the proof
     gen_proof(&bytes, overflow_checks, *blocksize);
+    // ===============================================================
+    // New style
+    // ===============================================================
+    let mut roots = HashMap::new();
+    let prefix = default_prefix(target);
+    roots.insert((0,0),"main".to_string());
+    // Disassemble bytes into instructions
+    let mut contract = Assembly::from_legacy_bytes(&bytes);
+    // Infer havoc instructions
+    contract = infer_havoc_insns(contract);
+    // Deconstruct into sequences
+    let blkseqs = deconstruct(&contract,*blocksize);
+    // Group subsequences
+    let groups = group(roots,&blkseqs);
+    //
+    write_headers(&prefix,&contract);
+    // Write files
+    write_groups(&prefix,groups);
     // Done
     Ok(())
 }
+
+fn default_prefix(name: &str) -> String {
+    name.replace(".","_")
+}
+
+struct BlockGroup {
+    id: usize,
+    name: String,
+    blocks: Vec<Block>
+}
+
+// Given an assembly, deconstruct it into a set of blocks of a given
+// maximum size.
+fn deconstruct(contract: &Assembly, blocksize: usize) -> Vec<BlockSequence> {
+    let mut blocks = Vec::new();
+    //
+    for s in contract {
+        match s {
+            StructuredSection::Code(insns) => {
+                blocks.push(BlockSequence::from_insns(blocksize,insns));
+            }
+            StructuredSection::Data(bytes) => {
+                // Nothing (for now)
+            }
+        }
+    }
+    //
+    blocks
+}
+
+// Given a sequence of blocks, generate a set of block groups.
+fn group(roots: HashMap<(usize,usize),String>, blocks: &[BlockSequence]) -> Vec<BlockGroup> {
+    let mut groups = Vec::new();
+    //
+    for (i,blk) in blocks.iter().enumerate() {
+        groups.extend(split(&roots,i,blk));
+    }
+    //
+    groups
+}
+
+/// Split a given sequence of blocks (in the same code segment) upto
+/// into one or more groups.
+fn split(roots: &HashMap<(usize,usize),String>, id: usize, blocks: &BlockSequence) -> Vec<BlockGroup> {
+    let name = roots.get(&(id,0x00)).unwrap().clone();
+    // HACK FOR NOW
+    let grp = BlockGroup{id,name,blocks: blocks.clone().to_vec()};
+    // Done?
+    vec![grp]
+}
+
+/// Convert each block group into a sequence of one or more files
+/// using a given prefix.
+fn write_groups(prefix: &str, groups: Vec<BlockGroup>) {
+    for g in groups {
+        let filename = format!("{prefix}_{}_{}.dfy",g.id,g.name);
+        println!("Writing {filename}");
+        // Construct block printer
+        let mut printer = DafnyPrinter::new(g.id);
+        //
+        for blk in g.blocks {
+            printer.print_block(&blk);
+        }
+        // Write the file
+        fs::write(filename,printer.to_string());
+    }
+}
+
+fn write_headers(prefix: &str, contract: &Assembly) {
+    for (i,s) in contract.iter().enumerate() {
+        match s {
+            StructuredSection::Code(insns) => {
+                let filename = format!("{prefix}_{}_header.dfy",i);
+                println!("Writing {filename}");        
+                // Construct block printer
+                let mut printer = DafnyPrinter::new(i);
+                // Print bytecode
+                printer.print_bytecode(insns);
+                // Write file
+                fs::write(filename,printer.to_string());        
+            }
+            StructuredSection::Data(bytes) => {
+                // Nothing (for now)
+            }
+        }
+    }    
+}
+
+
 
 // ===================================================================
 
@@ -68,12 +177,6 @@ fn gen_proof(bytes: &[u8], preconditions: PreconditionFn, blocksize: usize) {
                 }
                 //
                 println!("{}",printer.to_string());
-                // // Compute analysis results
-                // let init : State = State::new();
-                // // Run the abstract trace
-                // let states : Vec<Vec<State>> = trace(&insns,init);
-                // // Print out the code
-                // print_code_section(id, insns, &states, preconditions, blocksize)
             }
             StructuredSection::Data(bytes) => {
                 // For now.
